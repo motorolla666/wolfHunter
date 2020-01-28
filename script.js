0eaae11d60ec5f021c017706a42ee883b325dd43
motorolla666 <motorolla666@gmail.com>
2020-01-28 19:10:15 +0300

    add new files

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let counter = 0;
+let counterElement = document.getElementById("counter-element");
+let player = document.getElementById("pl");
+
+
+
+
+
+setInterval(() => {
+    let activeImgs = document.getElementsByClassName("active");
+    activeImgs[0].className = "";
+
+    let x = Math.floor(Math.random() * 25);
+    let allImgs = document.getElementsByTagName("img");
+    allImgs[x].className = "active";
+
+    allImgs[x].onclick = () => {
+        counter++;
+        counterElement.innerText = counter;
+        player.currentTime = 0;
+        player.play();
+
+    }
+}, 1700);
